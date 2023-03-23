b7d1f3bd78fe814d5de878b9d7d590a140d77cfd
Sergeindamix <86927266+Sergeindamix@users.noreply.github.com>
2023-03-22 19:43:12 -0600

    Update and rename install.py to setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='imgSeq2Vid',
+    name='yomero-image-to-video',
     version='0.1.0',
     description='Converts a sequence of images into a video',
     author='yomero',
