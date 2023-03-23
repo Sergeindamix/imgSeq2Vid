534a98bc74df2696fb079b477f47b5b0977799bc
Sergeindamix <86927266+Sergeindamix@users.noreply.github.com>
2023-03-22 19:29:47 -0600

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='yomero-image-to-video',
+    name='imgSeq2Vid',
     version='0.1.0',
     description='Converts a sequence of images into a video',
     author='yomero',
